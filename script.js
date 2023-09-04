bca85b10a4fe5fdbf5698047757619e0846415c4
Moahnsn <mohmmedvip708@gmail.com>
2023-09-04 19:54:52 +0300

    ز
    ز

---
+++ script.js
@@ -0,0 +1,18 @@
+ alert ("مرحباً بك في موقعي! أنا محمد، وسوف أحسب درجتك");
+ 
+ 
+ let grade = prompt("أدخل درجتك");
+
+console.log(grade);
+if( grade >= 90){console.log("لقد حصلت على امتياز 🥳");}
+else if (grade>=80){console.log("لقد حصلت على جيد جداً🤩")}
+else if (grade>=70){console.log("لقد حصلت على جيد🙂")}
+else if (grade>=60){console.log("لقد حصلت على مقبول😕")}
+else if (grade>=50){console.log( "لقد حصلت على ضعيف☹️")}
+else  {
+    console.log('راسب')
+}
+console.log('\x1b[36m%s\x1b[0m', 'GeeksforGeeks');
+console.log('\x1b[36m%s\x1b[0m', 'A computer science portal')
+
+//جافاسكربت ليست جافا/
